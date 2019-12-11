d7efd92159d6fd241687bbe9acd21b01bfc3842c
christianaasare <christiana.asare@turntabl.io>
2019-12-11 23:56:51 +0000

    New dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './screens/admin/components/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:AppComponent},
+  { path: 'admin', component:DashboardComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
