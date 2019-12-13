79cc9dcb469d0422361f97477ae42b9bf4bbe393
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-12-13 15:14:24 +0000

    changed the user redirect to a turntabl page

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post(
     failureFlash: false
   }),
   function(req, res) {
-    res.redirect("https://tpms-ui.herokuapp.com/admin?code="+dummy);
+    res.redirect("https://tpms-ui.herokuapp.com/admin?user="+dummy);
   }
 );
 
