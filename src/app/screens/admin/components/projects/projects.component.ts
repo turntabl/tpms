37677cd7eaf13f81815c81dc95abf23037044ae1
bill <teebill15@gmail.com>
2019-12-13 10:07:57 +0100

    added a form control to the module'

--- src/app/screens/admin/components/projects/projects.component.ts
+++ src/app/screens/admin/components/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
 import { MatIconRegistry } from "@angular/material/icon";
+import { FormControl } from '@angular/forms';
 @Component({
   selector: "app-projects",
   templateUrl: "./projects.component.html",
