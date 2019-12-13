8d617526d6728dd3dd67fd26698d981d680df9f1
bill <teebill15@gmail.com>
2019-12-13 14:36:05 +0100

    modified project service

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http/http';
+import { Observable } from 'rxjs';
+import { ProjectInterface } from './screens/project-interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
+  private projectUrl ="https://projectservice02.herokuapp.com/projects";
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+//  adding project to db
+addNewProject(client: ProjectInterface): Observable<ProjectInterface>{
+  return this.http.post<ProjectInterface>(this.projectUrl+ '/add',client );
+
+    }
 }
