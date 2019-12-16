d06bcad133ba35c9d09821b3d2b2a563cf20dd29
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-12-16 13:27:21 +0000

    injected username to all navbars from verify screen

--- src/app/screens/developer/nav/nav.component.ts
+++ src/app/screens/developer/nav/nav.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AppService } from "src/app/services/app.service";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  userName: string = "dot";
+  constructor(private appservice: AppService) {}
 
   ngOnInit() {
+    this.appservice.currentMessage.subscribe(
+      newName => (this.userName = newName)
+    );
   }
-
 }
--- src/app/screens/verify/verify.component.ts
+++ src/app/screens/verify/verify.component.ts
@@ -8,6 +8,7 @@ import { AppService } from "src/app/services/app.service";
 })
 export class VerifyComponent implements OnInit {
   isLoading: boolean = true;
+  userName: string;
   constructor(
     private router: Router,
     private appservice: AppService,
@@ -16,16 +17,19 @@ export class VerifyComponent implements OnInit {
   role: any;
   ngOnInit() {
     // console.log(this.activatedRoute.snapshot.params.name);
+    this.appservice.currentMessage.subscribe(name => (this.userName = name));
     this.appservice
       .getEmployeeRole(this.activatedRoute.snapshot.params.name)
       .subscribe(response => {
         // console.log("Response", response[0].emp_role);
         switch (response[0].emp_role) {
           case "admin":
+            this.appservice.changeMessage(response[0].emp_name);
             this.isLoading = false;
             this.router.navigate(["admin/projects"]);
             break;
           case "Developer":
+            this.appservice.changeMessage(response[0].emp_name);
             this.isLoading = false;
             this.router.navigate(["developer/projects"]);
             break;
