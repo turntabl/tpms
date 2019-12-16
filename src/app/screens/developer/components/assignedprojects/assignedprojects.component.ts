e644b146912e8df99fc87409b8a333cd70230253
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-12-16 15:44:34 +0000

    added mocked data from server for assigned projects

--- src/app/screens/developer/components/assignedprojects/assignedprojects.component.ts
+++ src/app/screens/developer/components/assignedprojects/assignedprojects.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
-
+import { Component, OnInit } from "@angular/core";
+import { ProjectInterface } from "src/app/screens/project-interface";
 
 @Component({
-  selector: 'app-assignedprojects',
-  templateUrl: './assignedprojects.component.html',
-  styleUrls: ['./assignedprojects.component.css']
+  selector: "app-assignedprojects",
+  templateUrl: "./assignedprojects.component.html",
+  styleUrls: ["./assignedprojects.component.css"]
 })
-
-
 export class AssignedprojectsComponent implements OnInit {
+  private assignedProjects: Array<ProjectInterface> = [
+    {
+      description: "Sample description",
+      project_id: 1,
+      title: "Tomato API in C#",
+      startdate: new Date(),
+      enddate: new Date(),
+      dev: 1
+    },
+    {
+      description: "Sample description",
+      project_id: 10,
+      title: "Heroku Pipelining",
+      startdate: new Date(),
+      enddate: new Date(),
+      dev: 1
+    }
+  ];
 
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  logsuccess(){
+    
   }
-
 }
