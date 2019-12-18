da7ed27658ca7871943b630abb3974b50fa97927
bill <teebill15@gmail.com>
2019-12-18 08:59:49 +0100

    modified UI for submitting dev logging hours

--- src/app/projectlogging.service.ts
+++ src/app/projectlogging.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Projectlogging } from './projectlogging';
+import { Observable, of } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectloggingService {
+  private projectsUrl = 'https://developerservice03.herokuapp.com/addhours';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor( private http: HttpClient) { }
+
+  getaddhours (): Observable<Projectlogging[]> {
+    return this.http.get<Projectlogging[]>(this.projectsUrl)
+    }
+  loghours(data: Projectlogging):Observable<Projectlogging>{
+    return this.http.post<Projectlogging>(this.projectsUrl, data);
+  }
 }
