edf50776420857f5531a7ab34b470e103443b852
FrancisBilla <francis.billa.fb@gmail.com>
2019-12-19 14:00:00 +0000

    adding project to db

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,29 +1,29 @@
-import { Injectable } from "@angular/core";
-import { HttpHeaders, HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { ProjectInterface } from "./screens/project-interface";
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProjectInterface } from './screens/project-interface';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class ProjectService {
-  private projectUrl = "https://projectservice02.herokuapp.com";
+  private projectUrl = 'https://projectservice02.herokuapp.com';
   httpOptions = {
-    headers: new HttpHeaders({ "Content-Type": "application/json" })
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
   constructor(private http: HttpClient) {}
   //  adding project to db
   addNewProject(body: ProjectInterface): Observable<ProjectInterface> {
     return this.http.post<ProjectInterface>(
-      this.projectUrl + "/projects",
+      this.projectUrl + '/projects',
       body
     );
   }
 
   getAssignedProject(empId: string): Observable<ProjectInterface> {
     return this.http.get<ProjectInterface>(
-      this.projectUrl + "/dev/assign/" + empId
+      this.projectUrl + '/dev/assign/' + empId
     );
   }
 }
