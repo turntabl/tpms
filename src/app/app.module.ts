f5caf3ed55cc7e0eb75e0190c23be7fa42ef49d2
bill <teebill15@gmail.com>
2019-12-19 17:08:54 +0000

    modified ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,8 @@ import { VerifyComponent } from "./screens/verify/verify.component";
 import { MatFormFieldModule, MatInputModule, MatNativeDateModule, DateAdapter, MAT_DATE_LOCALE } from "@angular/material";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { NavComponent } from './screens/developer/nav/nav.component';
+import { ViewChild, HostListener, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+
 
 @NgModule({
   declarations: [
@@ -74,6 +76,8 @@ import { NavComponent } from './screens/developer/nav/nav.component';
     MatToolbarModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    
+
 
      
 
