08603dd4f429edfcde3353de3dd467e1cbdf11f0
bill <teebill15@gmail.com>
2020-01-09 15:02:30 +0000

    removed selected dev name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 
 
+
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { DashboardComponent } from "./screens/admin/components/dashboard/dashboard.component";
