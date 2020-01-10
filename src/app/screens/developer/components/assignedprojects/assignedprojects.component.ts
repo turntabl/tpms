dab4f98f3567d4e8dc9389d0dc7b888fa24f7b1e
FrancisBilla <francis.billa.fb@gmail.com>
2020-01-10 11:15:14 +0000

    fixing bug: multi select field in hour log

--- src/app/screens/developer/components/assignedprojects/assignedprojects.component.ts
+++ src/app/screens/developer/components/assignedprojects/assignedprojects.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { MatTableDataSource } from "@angular/material/table";
-import { FormGroup, FormControl } from "@angular/forms";
-import { DomSanitizer } from "@angular/platform-browser";
-import { MatIconRegistry } from "@angular/material/icon";
-import { ProjectInterface } from "src/app/screens/project-interface";
-import { Projectlogging } from "src/app/projectlogging";
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import { ProjectInterface } from 'src/app/screens/project-interface';
+import { Projectlogging } from 'src/app/projectlogging';
 
-import { ProjectloggingService } from "src/app/projectlogging.service";
-import { ProjectService } from "src/app/project.service";
+import { ProjectloggingService } from 'src/app/projectlogging.service';
+import { ProjectService } from 'src/app/project.service';
 
 export interface PeriodicElement {
   title: string;
@@ -19,43 +19,43 @@ export interface PeriodicElement {
 }
 const ELEMENT_DATA: PeriodicElement[] = [
   {
-    title: "Web Services",
-    description: "Lorem ipsium",
-    startdate: "3.09.17",
-    enddate: "12.01.18",
+    title: 'Web Services',
+    description: 'Lorem ipsium',
+    startdate: '3.09.17',
+    enddate: '12.01.18',
     duration: 6,
     devno: 2
   }
 ];
 
 @Component({
-  selector: "app-assignedprojects",
-  templateUrl: "./assignedprojects.component.html",
-  styleUrls: ["./assignedprojects.component.css"]
+  selector: 'app-assignedprojects',
+  templateUrl: './assignedprojects.component.html',
+  styleUrls: ['./assignedprojects.component.css']
 })
 export class AssignedprojectsComponent implements OnInit {
-  incomingProject = { project_id: 2, title: "" };
+  incomingProject = { project_id: 2, title: '' };
   showAlert: boolean = false;
 
   hourform = new FormGroup({
-    project_hours: new FormControl(""),
-    volunteering_hours: new FormControl(""),
-    vacation: new FormControl(""),
-    sick: new FormControl(""),
-    emp_id: new FormControl(localStorage.getItem("empId")),
-    project_id: new FormControl(localStorage.getItem("pid")),
-    logged_date: new FormControl(new Date().toISOString().slice(0, 10))
+    project_hours: new FormControl(''),
+    volunteering_hours: new FormControl(''),
+    vacation: new FormControl(''),
+    sick: new FormControl(''),
+    emp_id: new FormControl(localStorage.getItem('empId')),
+    project_id: new FormControl(localStorage.getItem('pid')),
+    logged_date: new FormControl(new Date().toISOString().slice(0, 10)),
   });
   constructor(
     private plog: ProjectloggingService,
     private projectService: ProjectService
   ) {}
-  newProject = "";
+  newProject = '';
   displayedColumns: string[] = [
-    "Web Services",
-    "Volunteering",
-    "Sick",
-    "Vacation"
+    'Web Services',
+    'Volunteering',
+    'Sick',
+    'Vacation'
   ];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
   dummy = [];
@@ -66,43 +66,57 @@ export class AssignedprojectsComponent implements OnInit {
 
   ngOnInit() {
     this.projectService
-      .getAssignedProject(localStorage.getItem("empId"))
+      .getAssignedProject(localStorage.getItem('empId'))
       .subscribe(response => {
         // this.incomingProject.project_id = response.project_id;
-        localStorage.setItem("pid", response.project_id.toString());
+        localStorage.setItem('pid', response.project_id.toString());
         this.incomingProject.title = response.title;
       });
-    // this.projectService
-    //   .getAssignedProject(localStorage.getItem("empId"))
-    //   .subscribe(response => {
-    //     this.incomingProject.project_id = response.project_id;
-    //     this.incomingProject.title = response.title;
-    //   });
 
-    // test disability form here
-    // this.hourform.controls["sick"].disable();
   }
 
-  sickChecked(event) {
-    if (event == "sick") {
-      this.hourform.controls["vacation"].disable();
-      this.hourform.controls["volunteering_hours"].disable();
-      this.hourform.controls["project_hours"].disable();
+  sickFieldChecked(event) {
+    // tslint:disable-next-line: triple-equals
+    if (event == 'sick') {
+      this.hourform.controls.vacation.disable();
+      this.hourform.controls.volunteering_hours.disable();
+      this.hourform.controls.project_hours.disable();
     }
-    
+    else {
+      this.hourform.controls.vacation.enable();
+      this.hourform.controls.volunteering_hours.enable();
+      this.hourform.controls.project_hours.enable();
+    }
   }
-  // sickclicked() {
-  //   console.log(this.hourform.value.sick);
-  //   if (this.hourform.value.sick == "") {
-  //     this.hourform.controls["vacation"].disable();
-  //     this.hourform.controls["volunteering_hours"].disable();
-  //     this.hourform.controls["project_hours"].disable();
-  //   } else {
-  //     this.hourform.controls["vacation"].enable();
-  //     this.hourform.controls["volunteering_hours"].enable();
-  //     this.hourform.controls["project_hours"].enable();
-  //   }
-  // }
+
+  vacationFieldChecked(event) {
+    // tslint:disable-next-line: triple-equals
+    if (event == 'vacation') {
+      this.hourform.controls.sick.disable();
+      this.hourform.controls.volunteering_hours.disable();
+      this.hourform.controls.project_hours.disable();
+    }
+    else {
+      this.hourform.controls.sick.enable();
+      this.hourform.controls.volunteering_hours.enable();
+      this.hourform.controls.project_hours.enable();
+    }
+  }
+
+  submitChecked(event) {
+    // tslint:disable-next-line: triple-equals
+    if (event == 'true') {
+      this.hourform.controls.vacation.enable();
+      this.hourform.controls.sick.enable();
+      this.hourform.controls.volunteering_hours.enable();
+      this.hourform.controls.project_hours.enable();
+    }
+  }
+
+//   $scope.uncheck = function(event) {
+//     if ($scope.checked == event.target.value)
+//         $scope.checked = false
+// }
 
   logsuccess() {}
   onSubmit() {
@@ -114,6 +128,7 @@ export class AssignedprojectsComponent implements OnInit {
     //   this.showAlert = true;
     // });
     // alert(JSON.stringify(this.hourform.value));
+    this.hourform.reset();
   }
 
   myFilter = (d: Date): boolean => {
