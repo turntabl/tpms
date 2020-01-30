9aa55b285cf3d413d3e017417a2f635a4186e300
christianaasare <christiana.asare@turntabl.io>
2020-01-30 08:01:38 +0000

    resolving config file error

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    frameworks: ['mocha'],
+    // frameworks: ['mocha'],
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
