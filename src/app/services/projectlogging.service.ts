ac498758e25a6a4e8facc203c383fb2c9888c6b1
bill-n <teebill15@gmail.com>
2020-02-05 17:37:35 +0000

    modified more files

--- src/app/services/projectlogging.service.ts
+++ src/app/services/projectlogging.service.ts
@@ -15,16 +15,19 @@ export class ProjectloggingService {
   subscribe(res =>{sessionStorage.setItem('url',res.url)
   })
 }
+
   logproject(data: any):Observable<any>{
     let body = JSON.stringify(data);
     let headers = new HttpHeaders({'Content-Type':'application/json'});
     return this.http.post<any>(  sessionStorage.getItem('url') + '/v1/api/addloggedproject', body,{headers: headers});
   }
+
   logsick(data: any):Observable<any>{
     let body = JSON.stringify(data);
     let headers = new HttpHeaders({'Content-Type':'application/json'});
     return this.http.post<any>(  sessionStorage.getItem('url') + '/v1/api/addloggedsick', body,{headers: headers});
   }
+
   logvacation(data: any):Observable<any>{
     let body = JSON.stringify(data);
     let headers = new HttpHeaders({'Content-Type':'application/json'});
