d80648488902f78d3096347583751983fa299011
christianaasare <christiana.asare@turntabl.io>
2020-02-06 16:28:42 +0000

    fixing magic urls

--- src/app/services/projectlogging.service.ts
+++ src/app/services/projectlogging.service.ts
@@ -6,30 +6,31 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class ProjectloggingService {
-  private projectsUrl = 'https://project.services.turntabl.io';
-
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient)  {this.http.get<any>(window.location.origin +'/employee_service').
+  subscribe(res =>{sessionStorage.setItem('url',res.url)
+  })
+}
   logproject(data: any):Observable<any>{
     let body = JSON.stringify(data);
     let headers = new HttpHeaders({'Content-Type':'application/json'});
-    return this.http.post<any>(this.projectsUrl + '/v1/api/addloggedproject', body,{headers: headers});
+    return this.http.post<any>(sessionStorage.getItem('url') + '/v1/api/addloggedproject', body,{headers: headers});
   }
   logsick(data: any):Observable<any>{
     let body = JSON.stringify(data);
     let headers = new HttpHeaders({'Content-Type':'application/json'});
-    return this.http.post<any>(this.projectsUrl + '/v1/api/addloggedsick', body,{headers: headers});
+    return this.http.post<any>(sessionStorage.getItem('url')  + '/v1/api/addloggedsick', body,{headers: headers});
   }
   logvacation(data: any):Observable<any>{
     let body = JSON.stringify(data);
     let headers = new HttpHeaders({'Content-Type':'application/json'});
-    return this.http.post<any>(this.projectsUrl + '/v1/api/addloggedvaction', body,{headers: headers});
+    return this.http.post<any>(sessionStorage.getItem('url')  + '/v1/api/addloggedvaction', body,{headers: headers});
   }
   getLoggedHours(): Observable<any[]> {
-    return this.http.get<any[]>(this.projectsUrl + '/v1/api/getloggedhours');
+    return this.http.get<any[]>(sessionStorage.getItem('url') + '/v1/api/getloggedhours');
   }
 
 
