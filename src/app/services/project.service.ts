658e7f500d2695cc65de85c23e76f4df069b65cb
bill-n <teebill15@gmail.com>
2020-02-07 18:21:26 +0000

    modified some file

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -3,7 +3,6 @@ import { HttpHeaders, HttpClient, } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -57,5 +56,4 @@ export class ProjectService {
       this.url + "/v1/api/projects/assigned/employee/" + employee_id
     );
   }
-
 }
